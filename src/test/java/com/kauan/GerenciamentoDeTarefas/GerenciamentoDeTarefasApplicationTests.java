93b09727c432b5f1bb39328efc16f519ff4b0009
Kauan-Code19 <pkauprofissional@gmail.com>
2024-03-30 19:24:56 -0300

    projeto criado

---
+++ src/test/java/com/kauan/GerenciamentoDeTarefas/GerenciamentoDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauan.GerenciamentoDeTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
