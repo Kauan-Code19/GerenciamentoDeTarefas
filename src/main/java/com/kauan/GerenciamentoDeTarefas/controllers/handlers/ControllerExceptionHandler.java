0a57cd7ffe700c842e0b2bf9a24befd4017075f7
Kauan-Code19 <pkauprofissional@gmail.com>
2024-04-05 14:51:02 -0300

    tratamento de exceção na validação de dados.

--- src/main/java/com/kauan/GerenciamentoDeTarefas/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/kauan/GerenciamentoDeTarefas/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.kauan.GerenciamentoDeTarefas.controllers.handlers;
 
 import com.kauan.GerenciamentoDeTarefas.dtos.exceptions.CustomError;
+import com.kauan.GerenciamentoDeTarefas.dtos.exceptions.ValidationError;
 import com.kauan.GerenciamentoDeTarefas.services.exceptions.DatabaseException;
 import com.kauan.GerenciamentoDeTarefas.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -31,5 +34,20 @@ public class ControllerExceptionHandler {
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidException(MethodArgumentNotValidException e,
+                                                                       HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+        ValidationError validationError = new ValidationError(Instant.now(), status.value(),
+                "Dados inválidos", request.getRequestURI());
+
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            validationError.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(status).body(validationError);
     }
 }
