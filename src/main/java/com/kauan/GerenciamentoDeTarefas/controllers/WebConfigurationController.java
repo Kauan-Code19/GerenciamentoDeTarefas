721ca6031e54fcb47e337c0d9ae706eb14b7f57c
Kauan-Code19 <pkauprofissional@gmail.com>
2024-05-02 15:35:31 -0300

    login básico criado e configuração de CORS

---
+++ src/main/java/com/kauan/GerenciamentoDeTarefas/controllers/WebConfigurationController.java
@@ -0,0 +1,23 @@
+package com.kauan.GerenciamentoDeTarefas.controllers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigurationController {
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
